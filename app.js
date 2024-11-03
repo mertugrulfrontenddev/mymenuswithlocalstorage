57a0299cfa9272cf84e23e7e0edab4ee65caf96b
mertugrulfrontenddev <166544633+mertugrulfrontenddev@users.noreply.github.com>
2024-11-03 18:36:15 +0300

    newjs

--- app.js
+++ app.js
@@ -17,19 +17,23 @@ function addItems(e) {
 
   items.push(item);
 
-  /*   this function will be populate with mapping 
-our items array to create our ul li element and checkbox and label
- */
-
+ 
   populateList(items, myUl);
 
   localStorage.setItem("items", JSON.stringify(items));
   this.reset();
 }
 
+ /*   this function will be populate with mapping 
+our items array to create our ul li element and checkbox and label
+ */
+
+
 function populateList(plates = [], platesList) {
   let myPlates = plates
     .map((plate, index) => {
+      let upperPlateText =
+        plate.text.charAt(0).toUpperCase() + plate.text.slice(1).toLowerCase();
       return `
 
             <li>
@@ -38,7 +42,7 @@ function populateList(plates = [], platesList) {
             <input type="checkbox" data-index=${index} id="item${index}" ${
         plate.done ? "checked" : ""
       }/>
-           <label for="item${index}"> ${plate.text}</label>
+           <label for="item${index}"> ${upperPlateText}</label>
            <span > <a href="#" data-index=${index} class="deleteLink" ><i class="fa-regular fa-square-minus"></i></a></span>
 
            </div>
@@ -50,7 +54,12 @@ function populateList(plates = [], platesList) {
 
   platesList.innerHTML = myPlates;
 }
+//end
 
+
+
+
+//this function manage checkboxes check status
 function toggleDone(e) {
   if (e.target.tagName === "INPUT") {
     let checkBox = e.target;
@@ -63,35 +72,30 @@ function toggleDone(e) {
     populateList(items, myUl);
   }
 }
+//end
 
 
-function deleteItem(e){
 
+//this function is used for delete one item from our array.
+function deleteItem(e) {
   console.log(e.target);
-  if(e.target.tagName==="I"){
-
-
+  if (e.target.tagName === "I") {
     console.log(e.target);
-    let linkIndex=e.target.dataset.index;
+    let linkIndex = e.target.dataset.index;
 
-    items.splice(linkIndex,1);
+    items.splice(linkIndex, 1);
     localStorage.setItem("items", JSON.stringify(items));
 
     populateList(items, myUl);
-
-
   }
-
-
 }
 
+//end
 
-myForm.addEventListener("submit", addItems);
+myForm.addEventListener("submit", addItems);//this eventlistener for our form 
 
-myUl.addEventListener("click", deleteItem);
-myUl.addEventListener("click", toggleDone);
-myUl.addEventListener("click", deleteItem);
-
-
+myUl.addEventListener("click", deleteItem);// this eventlistener for our Ul element
+myUl.addEventListener("click", toggleDone);// this eventlistener for our Ul element for toggleDOne 
+myUl.addEventListener("click", deleteItem);//this eventlistener for manage deleting item from ullist
 
 populateList(items, myUl);
